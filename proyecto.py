7878329c718439cf5f45228fb997c7861f389bb1
C09580 <62049200+C09580@users.noreply.github.com>
2022-03-05 18:50:13 -0600

    version 1.9

--- proyecto.py
+++ proyecto.py
@@ -69,8 +69,7 @@ chart_1['cantones'] = chart_1.index
 st.markdown('2. GRAFICO BARRAS')
 
 # Graficación
-fig = px.bar(chart_1[['cantones','longitud_right']], 
-labels={'cantones':'Canton', 'longitud_right':'Longitud vial'})
+fig = px.bar(chart_1[['cantones','longitud_right']])
 st.plotly_chart(fig)
 
 
